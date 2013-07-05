87cf582f810933362445657f5e21d3ec77105c3a
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 13:45:20 -0500

    Added flashcard files

---
+++ flashcards.rb
@@ -0,0 +1,33 @@
+class Game
+  def start
+  end
+
+  def get_flashcard
+  end
+
+  def validate_guess
+  end
+
+  def respond_to_user_guess
+  end
+
+  def show_definition
+  end
+end
+
+class Deck
+  def initialize
+    @deck = []
+  end
+
+  def load
+  end
+end
+
+class Flashcard
+  def initialize(options = {})
+    @term = options[:term]
+    @definition = options[:definition]
+  end
+end
+
