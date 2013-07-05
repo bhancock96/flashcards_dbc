71a7be73c86ff2fcc91d40748bcfb8f0868f5ee0
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 16:26:45 -0500

    flashcards.rb

--- flashcards.rb
+++ flashcards.rb
@@ -1,15 +1,28 @@
 require_relative 'flashcard_deck.rb'
 
 class Game
+  attr_accessor :guess
   def initialize
+    puts "Enter file name, bro."
+    file_name = gets.chomp
     @current_card = []
     @guess = ''
-    @deck = Deck.new.load('flashcard_samples.txt')
+    @deck = Deck.new.load("#{file_name}")
   end
 
-  def start
+  def start 
     get_flashcard
     puts show_definition
+    until @guess == 'exit'
+      puts "Enter your guess:"
+      @guess = gets.chomp
+      break if @guess == 'skip'
+    if validate_guess
+      puts "Good guess"
+      get_flashcard
+      puts show_definition
+    end
+    end
   end
 
   def get_flashcard
@@ -30,7 +43,10 @@ class Game
 end
 
 
+
 game = Game.new()
 
+ until game.guess == 'exit'
+  game.start
+ end
 
-game.start
