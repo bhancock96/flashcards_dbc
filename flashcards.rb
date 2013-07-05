e1d82e941c27b25d66aee8cb841c226b9483af71
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 14:50:13 -0500

    Fixed game class

--- flashcards.rb
+++ flashcards.rb
@@ -1,17 +1,35 @@
+require_relative('flashcard_deck')
+
 class Game
+  def initialize(deck)
+    @current_card = []
+    @guess = ''
+    @deck = deck
+  end
+
   def start
+    get_flashcard
+    puts show_definition
   end
 
   def get_flashcard
+    @current_card = @deck[rand(@deck.size)]
   end
 
   def validate_guess
+    true if @guess == @current_card[:term]
   end
 
   def respond_to_user_guess
+
   end
 
   def show_definition
+    @current_card[:definition]
+  end
+
+  def show_term
+    @current_card[:term]
   end
 end
 
